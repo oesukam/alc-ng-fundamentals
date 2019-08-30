bec72596eae36c792aad843629f268cd6ebb5ad8
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-08-30 12:12:13 +0200

    add events-list component

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'events-app',
   // templateUrl: './app.component.html',
   template: `
     <h1>Hello world</h1>
     <img src="/assets/images/basic-shield.png" />
+    <events-list></events-list>
   `,
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  template: `
+    <div>
+      <h2>Event List</h2>
+    </div>
+  `
+})
+export class EventsListComponent {}
